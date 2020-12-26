14d955c966e03a29da679de757041288542ab3c1
fstar <huanghao2@xiao100.com>
2020-12-26 12:17:57 +0800

    update webpack.config.js and build

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,42 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// const TerserPlugin = require("terser-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
+function createPlugins(env) {
+  if (env.prod) {
+    return [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    ]
+  }
+  return [
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+  ]
+}
 
-module.exports = {
-  mode: 'development',
-  entry: './src/app.js',
-  output: {
-    filename: 'app.[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+module.exports = env => {
+  // use production config when `env.prod` is true
+  console.log('env:', env.prod ? 'production' : 'development')
 
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    }),
-  ],
-  // TODO: production config
-  /* optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()],
-  }, */
+  const config = {
+    mode: env.prod ? 'production' : 'development',
+    entry: './src/app.js',
+    output: {
+      filename: 'app.[contenthash].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+
+    devtool: env.prod ? false : 'inline-source-map',
+    devServer: {
+      contentBase: './dist',
+    },
+    plugins: createPlugins(env),
+    optimization: env.prod ? {
+      minimize: true,
+      minimizer: [new TerserPlugin()],
+    } : {},
+  }
+
+  return config
 }
