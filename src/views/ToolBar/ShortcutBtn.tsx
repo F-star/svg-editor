3a9c7da2ebbc0c2af3c129d54c46db8011a6f377
fstar <huanghao2@xiao100.com>
2021-02-03 12:52:11 +0800

    style: Keyboard Shortcut Dialog

--- src/views/ToolBar/ShortcutBtn.tsx
+++ src/views/ToolBar/ShortcutBtn.tsx
@@ -16,8 +16,8 @@ import Button from '@material-ui/core/Button'
 import Dialog from '@material-ui/core/Dialog'
 import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
-import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
+import styled from 'styled-components'
 
 type State = {
   open: boolean,
@@ -35,6 +35,64 @@ class ShortcutBtn extends React.Component<any, State> {
   }
 
   render() {
+    const StyledColumn = styled.div`
+      border-bottom: 1px solid #eee;
+      width: 300px;
+      height: 36px;
+      line-height: 36px;
+      font-size: 14px;
+      overflow: hidden;
+      backgroundColor: #555;
+      &:last-child {
+        border-bottom: none;
+      }
+    `
+    const StyledLeftItem = styled.span`
+      float: left;
+      font-weight: 500;
+    `
+    const StyledRightItem = styled.span`
+      float: right;
+    `
+    const StyledKbd = styled.kbd`
+      background: linear-gradient(180deg, #eee, #fff);
+      background-color: #eee;
+      border: 1px solid #cdd5d7;
+      border-radius: 4px;
+      box-shadow: 0 1px 2px 1px #cdd5d7;
+      font-family: consolas,"Liberation Mono",courier,monospace;
+      font-size: 14px;
+      font-weight: 700;
+      line-height: 1;
+      margin: 3px;
+      padding: 3px 6px;
+      white-space: nowrap;
+    `
+
+    const ShortcutList = [
+      {
+        command: 'Undo',
+        KeyBinding: <><StyledKbd>Ctrl/Cmd</StyledKbd> + <StyledKbd>Z</StyledKbd></>
+      },
+      {
+        command: 'Redo',
+        KeyBinding: <><StyledKbd>Ctrl/Cmd</StyledKbd> + <StyledKbd>Shift</StyledKbd> + <StyledKbd> Y</StyledKbd></>
+      },
+      {
+        command: 'Delete Elements',
+        KeyBinding: <><StyledKbd>Backspace</StyledKbd></>,
+      },
+    ].map(item => {
+      return (
+        <StyledColumn key={item.command}>
+          <StyledLeftItem>{ item.command }</StyledLeftItem>
+          <StyledRightItem>
+            {item.KeyBinding}
+          </StyledRightItem>
+        </StyledColumn>
+      )
+    })
+
     return (
       <>
         <div
@@ -53,7 +111,6 @@ class ShortcutBtn extends React.Component<any, State> {
         >
         Keyboard
         </div>
-
         <Dialog
           open={this.state.open}
           onClose={() => { this.handleClose() }}
@@ -62,11 +119,7 @@ class ShortcutBtn extends React.Component<any, State> {
         >
           <DialogTitle id="alert-dialog-title">{'Keyboard Shortcuts'}</DialogTitle>
           <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              Undo: CtrlOrCmd + Z <br></br>
-              Redo: CtrlOrCmd + Shift + Z<br></br>
-              Delete: Backspace<br></br>
-            </DialogContentText>
+            {ShortcutList}
           </DialogContent>
           <DialogActions>
             <Button onClick={() => { this.handleClose() }} color="primary">
