1bcb3b9a660bea132a583d3a828c2125189df7a9
fstar <hhbuhuiai@163.com>
2021-05-08 00:11:36 +0800

    hudManager 改为 huds

--- src/modules/select/modes/SelectAreaMode.ts
+++ src/modules/select/modes/SelectAreaMode.ts
@@ -8,15 +8,15 @@ class SelectAreaMode extends Mode {
     const { x: endX, y: endY } = ctx.getPos()
     const { x: startX, y: startY } = ctx.getStartPos()
     const { x, y, w, h } = getBoxBy2points(startX, startY, endX, endY)
-    this.editor.hudManager.selectArea.drawRect(x, y, w, h)
+    this.editor.huds.selectArea.drawRect(x, y, w, h)
   }
   end() {
-    const box = this.editor.hudManager.selectArea.getBox()
-    this.editor.hudManager.selectArea.clear()
+    const box = this.editor.huds.selectArea.getBox()
+    this.editor.huds.selectArea.clear()
     this.editor.activedElsManager.setElsInBox(box)
   }
   endOutside() {
-    this.editor.hudManager.selectArea.clear()
+    this.editor.huds.selectArea.clear()
     this.editor.activedElsManager.clear()
   }
 }
